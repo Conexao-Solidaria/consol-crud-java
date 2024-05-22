0a9d2f5b432e852a2ed47257308456f8a8d78fd3
caioarnonii <caio.bernardo@sptech.school>
2024-05-21 21:09:34 -0300

    feat(DonatarioAtualizacaoTest):testes concluidos

--- src/test/java/com/consol/api/donatario/DonatarioAtualizacaoTest.java
+++ src/test/java/com/consol/api/donatario/DonatarioAtualizacaoTest.java
@@ -39,7 +39,7 @@ public class DonatarioAtualizacaoTest {
         @Test
         @SqlGroup(
                 @Sql(scripts = FILL_DATABASE_SCRIPT,
-                        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+                        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
         )
         @DisplayName("Deve retornar 200 e atualizar um donatario")
         public void teste1() throws Exception {
@@ -74,16 +74,16 @@ public class DonatarioAtualizacaoTest {
                     var donatario = """
                             {
                                 "nome": "",
-                                "cpf": "12345678901",
-                                "dataNascimento": "1990-01-01"
+                                "cpf": "45678901234",
+                                "dataNascimento": "1988-12-05"
                             }""";
 
-                    mockMvc.perform(put(URL + "3")
+                    mockMvc.perform(put(URL + "4")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(donatario))
                             .andExpect(status().isBadRequest())
                             .andExpect(jsonPath("$.message")
-                                    .value("Nome é obrigatório"));
+                                    .value("Nome eh obrigatorio"));
                 }
 
 //        retorna 400 quando cpf em branco
@@ -99,22 +99,138 @@ public class DonatarioAtualizacaoTest {
                         "dataNascimento": "1988-12-05"
                     } """;
 
-                    mockMvc.perform(put(URL + "3")
+                    mockMvc.perform(put(URL + "4")
                             .contentType(MediaType.APPLICATION_JSON)
                             .content(donatario)).andExpect(status().isBadRequest())
                             .andExpect(jsonPath("$.message")
-                            .value("CPF é obrigatório"));
+                            .value("CPF eh obrigatorio"));
                 }
 
-//        retorna 404 quando id nao encontrado
+        //        retorna 404 quando id nao encontrado
+
+                @Test
+                @SqlGroup(
+                        @Sql(scripts = FILL_DATABASE_SCRIPT,
+                                executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+                )
+                @DisplayName("Deve retornar 404 quando o Id não for encontrado")
+                public void teste3() throws Exception {
+
+                    var donatario = """
+                    {
+                        "nome": "Ana Santos",
+                        "cpf": "45678901234",
+                        "dataNascimento": "1988-12-05"
+                    } """;
+
+                    mockMvc.perform(put(URL + "400")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(donatario)).andExpect(status().isNotFound())
+                            .andExpect(jsonPath("$.message")
+                            .value("Donatario nao encontrado"));
+                }
+
 
 //        retorna 400 quando nao houver id
 
+        @Test
+        @DisplayName("Deve retornar 400 quando não houver id")
+        public void teste4() throws Exception {
+
+            var donatario = """
+                    {
+                        "nome": "Ana Santos",
+                        "cpf": "45678901234",
+                        "dataNascimento": "1988-12-05"
+                    } """;
+
+            mockMvc.perform(put(URL)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(donatario)).andExpect(status().isBadRequest())
+                            .andExpect(jsonPath("$.message")
+                            .value("Id invalido"));
+        }
+
+
 //        retorna 400 quando dataNascimento em branco
+
+        @Test
+        @DisplayName("Deve retornar 400 quando a data de nascimento estiver em branco")
+        public void teste5() throws Exception {
+
+            var donatario = """
+                    {
+                        "nome": "Ana Santos",
+                        "cpf": "45678901234",
+                        "dataNascimento": ""
+                    } """;
+
+            mockMvc.perform(put(URL + "4")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(donatario)).andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.message")
+                            .value("A data de nascimento é obrigatória"));
+        }
 
 //        Deve retornar 404 quando nao houver donatarios cadastrados
 
+        @Test
+        @DisplayName("Deve retornar 404 quando não houver donatarios cadastrados")
+        public void teste6() throws Exception {
+
+            var donatario = """
+                    {
+                        "nome": "Ana Santos",
+                        "cpf": "45678901234",
+                        "dataNascimento": "1988-12-05"
+                    } """;
+
+            mockMvc.perform(put(URL + "4")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(donatario)).andExpect(status().isNotFound())
+                            .andExpect(jsonPath("$.message")
+                            .value("Nao ha donatarios cadastrados"));
+        }
+
+
 //        retorna 400 quando cpf invalido
+
+        @Test
+        @DisplayName("Deve retornar 400 quando o cpf for inválido")
+        public void teste7() throws Exception {
+
+            var donatario = """
+                    {
+                        "nome": "Ana Santos",
+                        "cpf": "4567890123",
+                        "dataNascimento": "1988-12-05"
+                    } """;
+
+            mockMvc.perform(put(URL + "4")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(donatario)).andExpect(status().isBadRequest())
+                            .andExpect(jsonPath("$.message")
+                            .value("CPF invalido"));
+        }
+
+
 //        retorna 400 quando dataNascimento invalida
-    }
+@Test
+@DisplayName("Deve retornar 400 quando a data de nascimento estiver invalida")
+public void teste8() throws Exception {
+
+    var donatario = """
+                    {
+                        "nome": "Ana Santos",
+                        "cpf": "45678901234",
+                        "dataNascimento": "1988-12-0"
+                    } """;
+
+    mockMvc.perform(put(URL + "4")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(donatario)).andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.message")
+                    .value("Data de nascimento invalida"));
 }
+    }
+        }
