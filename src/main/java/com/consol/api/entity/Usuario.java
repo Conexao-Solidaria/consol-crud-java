bf0abce8b195742d2525632b19ed73338cf738cc
rafael_scheneider <rafael.ranaudo@sptech.school>
2024-06-04 20:52:40 -0300

    feat(): finalizando teste de usuario

--- src/main/java/com/consol/api/entity/Usuario.java
+++ src/main/java/com/consol/api/entity/Usuario.java
@@ -1,2 +1,30 @@
-package com.consol.api.entity;public class Usuario {
+package com.consol.api.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_usuario")
+    private int id;
+
+    private int coordenador;
+
+    private String nomeUsuario;
+
+    private String email;
+
+    private String senha;
+
+    private String cpf;
+
+    @ManyToOne
+    private Instituicao instituicao;
 }
--- src/main/java/com/consol/api/service/UsuarioService.java
+++ src/main/java/com/consol/api/service/UsuarioService.java
@@ -1,2 +1,24 @@
-package com.consol.api.service;public class UsuarioService {
+package com.consol.api.service;
+
+import com.consol.api.entity.Usuario;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+
+public class UsuarioService {
+    public Usuario porIdInst(){
+        return null;
+    }
+
+    public Usuario salvar(Usuario usuario){
+        return null;
+    }
+
+    public Usuario atualizar(int id, Usuario usuario){
+        return null;
+    }
+
+    public Boolean deletar(int id){
+        return null;
+    }
 }
