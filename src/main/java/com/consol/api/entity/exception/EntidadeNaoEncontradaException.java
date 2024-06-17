3d7faf323569651535cfb74d2606461a838b4996
LuannaDiStefani7 <luanna.souza@sptech.school>
2024-06-17 17:58:55 -0300

    fix(): Correção Atualização

--- src/main/java/com/consol/api/entity/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/consol/api/entity/exception/EntidadeNaoEncontradaException.java
@@ -1,4 +1,14 @@
 package com.consol.api.entity.exception;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntidadeNaoEncontradaException extends RuntimeException{
+        public EntidadeNaoEncontradaException(){
+
+        }
+
+
 }
