7dc02864799238d8bdcb449f17df4c61e6844f92
LuannaDiStefani7 <luanna.souza@sptech.school>
2024-09-05 18:25:38 -0300

    Refactor: Ajuste na exception de entidade não encontrada (404)

--- src/main/java/com/consol/api/entity/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/consol/api/entity/exception/EntidadeNaoEncontradaException.java
@@ -5,5 +5,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntidadeNaoEncontradaException extends RuntimeException{
-    public EntidadeNaoEncontradaException(){super("Entidade Não encontrada");}
+    public EntidadeNaoEncontradaException(String entidade){super(String.format("%s não entrado(a)", entidade));}
 }
