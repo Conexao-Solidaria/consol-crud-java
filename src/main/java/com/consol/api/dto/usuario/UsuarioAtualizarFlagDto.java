e64abf8759041c113f7b7dca6b4c1cf16b91aa4c
LuannaDiStefani7 <luanna.souza@sptech.school>
2024-10-04 20:50:26 -0300

    Fix(Usuario): Conversão do tipo para Byte

--- src/main/java/com/consol/api/dto/usuario/UsuarioAtualizarFlagDto.java
+++ src/main/java/com/consol/api/dto/usuario/UsuarioAtualizarFlagDto.java
@@ -1,12 +1,13 @@
 package com.consol.api.dto.usuario;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UsuarioAtualizarFlagDto {
     @Size(min = 1, max = 1)
-    @NotBlank(message = "A flag aprovado não pode estar em branco")
-    private Integer flagAprovado;
+    @NotNull
+    private Byte flagAprovado;
 }
