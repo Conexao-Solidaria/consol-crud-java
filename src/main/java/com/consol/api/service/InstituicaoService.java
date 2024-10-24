7e5e1f3fa7259484f9ccbd0ee8487dbfba180afb
LuannaDiStefani7 <luanna.souza@sptech.school>
2024-10-24 18:32:59 -0300

    Delete(Instituicao): Removendo instituicao

--- src/main/java/com/consol/api/service/InstituicaoService.java
+++ src/main/java/com/consol/api/service/InstituicaoService.java
@@ -1,58 +1,58 @@
-package com.consol.api.service;
-
-
-import com.consol.api.entity.Instituicao;
-import com.consol.api.entity.exception.RequisicaoIncorretaException;
-import com.consol.api.repository.InstituicaoRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-public class InstituicaoService {
-
-        private final InstituicaoRepository repository;
-
-        public Instituicao criar(Instituicao instituicao) {
-            return repository.save(instituicao);
-        }
-
-        public List<Instituicao> listarInstituicoes() {
-            return repository.findAll();
-        }
-
-        public Instituicao consultarPorId(Integer id) {
-            return repository.findById(id).orElseThrow(
-                    () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
-            );
-        }
-
-        public Instituicao atualizar(Integer id, Instituicao instituicao) {
-            Instituicao instituicaoAtualizada = consultarPorId(id);
-
-            instituicaoAtualizada.setNome(instituicao.getNome());
-            instituicaoAtualizada.setCep(instituicao.getCep());
-            instituicaoAtualizada.setNumeroImovel(instituicao.getNumeroImovel());
-            instituicaoAtualizada.setDescricao(instituicao.getDescricao());
-            instituicaoAtualizada.setFotoPerfil(instituicao.getFotoPerfil());
-
-            return repository.save(instituicaoAtualizada);
-        }
-
-        public void apagarPorId(Integer id) {
-            if (!repository.existsById(id)){
-                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-            }
-            repository.deleteById(id);
-        }
-
-        public boolean existById(int id){
-            return repository.existsById(id);
-        }
-
-    }
-
+//package com.consol.api.service;
+//
+//
+//import com.consol.api.entity.Instituicao;
+//import com.consol.api.entity.exception.RequisicaoIncorretaException;
+//import com.consol.api.repository.InstituicaoRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.server.ResponseStatusException;
+//
+//import java.util.List;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class InstituicaoService {
+//
+//        private final InstituicaoRepository repository;
+//
+//        public Instituicao criar(Instituicao instituicao) {
+//            return repository.save(instituicao);
+//        }
+//
+//        public List<Instituicao> listarInstituicoes() {
+//            return repository.findAll();
+//        }
+//
+//        public Instituicao consultarPorId(Integer id) {
+//            return repository.findById(id).orElseThrow(
+//                    () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+//            );
+//        }
+//
+//        public Instituicao atualizar(Integer id, Instituicao instituicao) {
+//            Instituicao instituicaoAtualizada = consultarPorId(id);
+//
+//            instituicaoAtualizada.setNome(instituicao.getNome());
+//            instituicaoAtualizada.setCep(instituicao.getCep());
+//            instituicaoAtualizada.setNumeroImovel(instituicao.getNumeroImovel());
+//            instituicaoAtualizada.setDescricao(instituicao.getDescricao());
+//            instituicaoAtualizada.setFotoPerfil(instituicao.getFotoPerfil());
+//
+//            return repository.save(instituicaoAtualizada);
+//        }
+//
+//        public void apagarPorId(Integer id) {
+//            if (!repository.existsById(id)){
+//                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+//            }
+//            repository.deleteById(id);
+//        }
+//
+//        public boolean existById(int id){
+//            return repository.existsById(id);
+//        }
+//
+//    }
+//
