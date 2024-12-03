ad7444084d77ea8e7b4106f551085abee6479716
Rafaqwert <rafael.rrodrigues@sptech.school>
2024-12-03 18:27:08 -0300

    feat(): adiciona healthcheck

--- src/main/java/com/consol/api/controller/HealthcheckController.java
+++ src/main/java/com/consol/api/controller/HealthcheckController.java
@@ -1,1 +1,13 @@
+package com.consol.api.controller;
 
+@RestController
+@RequestMapping("/healthcheck")
+@RequiredArgsConstructor
+public class FamiliaController {
+
+    private final FamiliaService service;
+
+    @GetMapping
+    public ResponseEntity<Void> getHealth() {
+        return ResponseEntity.ok().build();
+    }
