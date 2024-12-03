f57c0d8aa1e1637ff4b30906e4a2b18c3c3476cc
MatheusRequena <matheus.requena@sptech.school>
2024-12-03 18:03:02 -0300

    feat(): adiciona healthcheck

--- src/main/java/com/consol/api/configuration/security/SecurityConfiguracao.java
+++ src/main/java/com/consol/api/configuration/security/SecurityConfiguracao.java
@@ -54,7 +54,8 @@ public class SecurityConfiguracao {
             new AntPathRequestMatcher("/instituicoes/**"),
             new AntPathRequestMatcher("/h2-console/**"),
             new AntPathRequestMatcher("/h2-console/**/**"),
-            new AntPathRequestMatcher("/error/**")
+            new AntPathRequestMatcher("/error/**"),
+            new AntPathRequestMatcher("/healthcheck/**")
     };
 
     @Bean
