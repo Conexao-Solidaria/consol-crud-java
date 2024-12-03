cebe138b509cdc7081ac9d75e6999096dcaead60
eduardoseba05 <eduardo.seba@sptech.school>
2024-12-03 18:46:25 -0300

    feat(): ajuste healthcheck

--- src/main/java/com/consol/api/controller/HealthcheckController.java
+++ src/main/java/com/consol/api/controller/HealthcheckController.java
@@ -1,13 +1,18 @@
 package com.consol.api.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/healthcheck")
 @RequiredArgsConstructor
-public class FamiliaController {
-
-    private final FamiliaService service;
+public class HealthcheckController {
 
     @GetMapping
     public ResponseEntity<Void> getHealth() {
         return ResponseEntity.ok().build();
     }
+}
